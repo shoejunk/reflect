dbe6d4e20ca1768a37947f5c03ab481b743ad71a
Joseph Shunk <joseph.shunk@gmail.com>
2023-09-09 01:43:12 -0700

    Add parameters to construction in reflection.

--- include/reflect/reflect.h
+++ include/reflect/reflect.h
@@ -6,19 +6,66 @@
 
 namespace NStk::NReflect
 {
+	template<typename T>
+	class TDatum;
+
+	class CDatum
+	{
+	public:
+		template<typename T>
+		T& Get()
+		{
+			return static_cast<TDatum<T>*>(this)->Get();
+		}
+
+		template<typename T>
+		T const& Get() const
+		{
+			return static_cast<TDatum<T> const*>(this)->Get();
+		}
+	};
+
+	template<typename T>
+	class TDatum : public CDatum
+	{
+	public:
+		T& Get()
+		{
+			return m_mDatum;
+		}
+
+		T const& Get() const
+		{
+			return m_mDatum;
+		}
+
+	private:
+		T m_mDatum;
+	};
+
 	class CClassBase
 	{
 	public:
 		virtual void Construct() = 0;
+		virtual void Construct(std::vector<CDatum*> const& kaData) = 0;
 		virtual std::string const& GetClassName() const = 0;
 	};
 
-	template<class T>
+	template<typename T>
 	class TClass : public CClassBase
 	{
 	public:
 		TClass<T>(std::string sClassName) : m_ksClassName(sClassName) {}
+
 		virtual void Construct() override
+		{
+			m_aObjects.push_back(std::make_unique<T>());
+		}
+
+		// Construct with a vector of data. By default, this just calls the default constructor.
+		// In order to use the data, you must override this function in a template specialization
+		// of TClass.
+		virtual void Construct(std::vector<CDatum*> const& kaData) override
 		{
 			m_aObjects.push_back(std::make_unique<T>());
 		}
@@ -36,7 +83,7 @@ namespace NStk::NReflect
 	class CReflect
 	{
 	public:
-		template<class T>
+		template<typename T>
 		void Register(std::string sClassName)
 		{
 			m_aClasses[sClassName] = std::make_unique<TClass<T>>(sClassName);
@@ -45,6 +92,11 @@ namespace NStk::NReflect
 		void Construct(std::string const& ksClassName)
 		{
 			m_aClasses[ksClassName]->Construct();
+		}
+
+		void Construct(std::string const& ksClassName, std::vector<CDatum*> const& kaData)
+		{
+			m_aClasses[ksClassName]->Construct(kaData);
 		}
 
 	private:
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -11,12 +11,39 @@ class CTest
 {
 public:
 	CTest() { Log("Hello World!\n"); }
+	CTest(int32_t i)
+	{
+		Log("Hello World %d!\n", i);
+	}
 };
+
+template<>
+void TClass<CTest>::Construct(std::vector<CDatum*> const& kaData)
+{
+	if (kaData.size() == 0)
+	{
+		m_aObjects.push_back(std::make_unique<CTest>());
+	}
+	else if (kaData.size() == 1)
+	{
+		int32_t i = kaData[0]->Get<int32_t>();
+		m_aObjects.push_back(std::make_unique<CTest>(i));
+	}
+	else
+	{
+		Log("Error: Too many arguments to construct CTest!\n");
+	}
+}
+
 
 int main()
 {
 	CReflect oReflect;
 	oReflect.Register<CTest>("Test");
-	oReflect.Construct("Test");
+	TDatum<int32_t> oDatum;
+	oDatum.Get() = 5;
+	std::vector<CDatum*> aData{ &oDatum };
+	int32_t i = aData[0]->Get<int32_t>();
+	oReflect.Construct("Test", aData);
 	return 0;
 }
